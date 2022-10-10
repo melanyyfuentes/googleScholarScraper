709301bd5d23b94d1094555edad3b9649f44a48d
melanybarocio <83933941+melanybarocio@users.noreply.github.com>
2022-10-10 17:04:00 -0400

    Setting up make_pretty for cleanings

--- scraper.py
+++ scraper.py
@@ -2,10 +2,14 @@
 Melany Fuentes
 Google Scholar Web Scraper
 """
+import argparse
+from datetime import datetime
+from time import strftime
 
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import csv
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -16,7 +20,6 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 
-
 # spoofing headers to look legit
 headers = {
     'Access-Control-Allow-Origin': '*',
@@ -24,8 +27,7 @@ headers = {
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Max-Age': '3600',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
-
+}
 
 
 # To theoretically go through a list of names, search it
@@ -33,84 +35,78 @@ headers = {
 # put this into a csv file
 
 
-def get_urls(querylist):
-    df = pd.DataFrame(columns=['person', 'user_url'])
+def get_urls(querylist, save_url):
+    user_data = []
+    # making data frame
+    url_frame = pd.DataFrame(columns=['person', 'user_url'])
 
     for query in querylist:
-
-        url = 'https://scholar.google.com/citations?view_op=search_authors&mauthors=' + query + '&hl=en&oi=ao'
+        url = 'https://scholar.google.com/citations?view_op=search_authors&mauthors=' + query.replace(" ",
+                                                                                                      "+") + '&hl=en&oi=ao'
         req = requests.get(url, headers)
         soup = BeautifulSoup(req.content, 'html.parser')
 
         # gives results from search query
         person_results = soup.find_all(class_='gs_ai_pho')
-        print('persons results:' + str(person_results[0]))
+        print(type(person_results))
 
-        # appending first URL and name to dataframe
-        person_url = person_results[0].get('href')
-        df.loc[len(df.index)] = [query, person_url]
-        #for cell in soup.select('table#foobar td.empformbody'):
-    # Do something with these table cells.
-        get_info(person_url)
+        if len(person_results) == 0:
+            url_frame.loc[len(url_frame.index)] = [query, 'NA']
+        else:
+            url_got = 'https://scholar.google.com' + person_results[0].get('href')
+            url_frame.loc[len(url_frame.index)] = [query, url_got]
+
+    url_frame.to_csv(save_url, index=False)
 
 
-
-def get_info(user_url):
-    url = 'https://scholar.google.com' + str(user_url)
-
+def get_info(user_urls):
     options = Options()
-    options.add_argument("start-maximized")
+    options.add_argument("start-maximized")                                                      info_frame = pd.DataFrame(columns=['author', 'paper_title', 'paper_link', 'year', 'coauthors', 'publish_type', 'pages'])
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.get(url)
-
-    more = True
-    #while more:
-    elem = driver.find_element('xpath', '//*[@id="gsc_bpf_more"]')
-
-    # just clicking through, make this nicer later
-    elem.click()
-    elem.click()
-    elem.click()
-    elem.click()
-    elem.click()
-    elem.click()
-    elem.click()
-    elem.click()
-
-    page = driver.page_source
-
-    soup = BeautifulSoup(page, 'html.parser')
-
-    #print(soup)
-
-    results = soup.find_all(class_='gsc_a_tr')
+    with open(user_urls, "rb") as f:
+        mycsv = csv.reader(f)
+        for row in mycsv:
+            if row[1] != "NA":
+                url = 'https://scholar.google.com' + str(row[1])
+                driver.get(url)
+                more = True
+                #while more:
+                elem = driver.find_element('xpath', '//*[@id="gsc_bpf_more"]')# just clicking through, make this nicer later
+                # clicking through the more thing
+                elem.click()
+                elem.click()
+                elem.click()
+                elem.click()
+                elem.click()
+                elem.click()
+                elem.click()
+                elem.click()
+                page = driver.page_source
 
 
+                soup = BeautifulSoup(page, 'html.parser')
+                results = soup.find_all(class_='gsc_a_tr')
 
-    for element in results:
+def make_pretty(html_code)
 
-
-    print(results[0])
-
-
-    # gives results from the
-
-
-
-
-
-
-
-
-
-
-
-#print(soup.prettify())
 
 if __name__ == '__main__':
-    # Get names and top URL
-    querys = ['Dorothy+Roberts'] # edit the list here
-    get_urls(querys)
+    # needed arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--authorData', default='data/sampleData.xlsx')
+    parser.add_argument('--columnName', default='Name')
+
+    args = parser.parse_args()
+
+    # Getting names
+    df = pd.ExcelFile(args.authorData).parse('Sheet1')
+    queries = [x for x in df[args.columnName]]
+
+    # getting URLs and saving into dataframe
+    saveDir = "data/" + str(datetime.now().strftime("%m.%d.%Y_%H:%M:%S")) + ".csv"
+    get_urls(queries, saveDir)
+
+    get_info()
 
     # Take existing dataframe URLs and get csv entries
-
+    # get_info()
