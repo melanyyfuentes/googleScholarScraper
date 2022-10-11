fc7470f1dac5e62eed7e74179b6638519e29dbaf
melanybarocio <83933941+melanybarocio@users.noreply.github.com>
2022-10-11 13:18:36 -0400

    Settings currently not working for getting paper info

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import time
+from selenium import webdriver
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe"))  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/')
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@ Google Scholar Web Scraper
 """
 import argparse
 from datetime import datetime
-from time import strftime
 
 from bs4 import BeautifulSoup
 import requests
@@ -12,15 +11,12 @@ import pandas as pd
 import csv
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
 
-# spoofing headers to look legit
+# spoofing headers for bs4 to look legit
 headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET',
@@ -33,7 +29,6 @@ headers = {
 # To theoretically go through a list of names, search it
 # then get the top url from the search
 # put this into a csv file
-
 
 def get_urls(querylist, save_url):
     user_data = []
@@ -48,7 +43,6 @@ def get_urls(querylist, save_url):
 
         # gives results from search query
         person_results = soup.find_all(class_='gs_ai_pho')
-        print(type(person_results))
 
         if len(person_results) == 0:
             url_frame.loc[len(url_frame.index)] = [query, 'NA']
@@ -59,35 +53,43 @@ def get_urls(querylist, save_url):
     url_frame.to_csv(save_url, index=False)
 
 
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+
+
 def get_info(user_urls):
-    options = Options()
-    options.add_argument("start-maximized")                                                      info_frame = pd.DataFrame(columns=['author', 'paper_title', 'paper_link', 'year', 'coauthors', 'publish_type', 'pages'])
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    with open(user_urls, "rb") as f:
-        mycsv = csv.reader(f)
-        for row in mycsv:
-            if row[1] != "NA":
-                url = 'https://scholar.google.com' + str(row[1])
+    csv_use = open(user_urls, "r")
+    mycsv = csv.reader(csv_use)
+    next(mycsv)
+    for row in mycsv:
+        print(row)
+        if row[1] != "NA":
+            url = 'https://scholar.google.com' + str(row[1])
+            driver = webdriver.Chrome('/Users/melanyfuentes/PycharmProjects/googleScholarScraper/chromedriver', options=chrome_options)
+            try:
                 driver.get(url)
-                more = True
-                #while more:
-                elem = driver.find_element('xpath', '//*[@id="gsc_bpf_more"]')# just clicking through, make this nicer later
-                # clicking through the more thing
-                elem.click()
-                elem.click()
-                elem.click()
-                elem.click()
-                elem.click()
-                elem.click()
-                elem.click()
-                elem.click()
-                page = driver.page_source
+            except WebDriverException:
+                print("page down")
+
+            elem = driver.find_element('xpath',
+                                       '//*[@id="gsc_bpf_more"]')  # just clicking through, make this nicer later
+            # clicking through the more thing
+            elem.click()
+            elem.click()
+            elem.click()
+            elem.click()
+            elem.click()
+            elem.click()
+            elem.click()
+            elem.click()
+            page = driver.page_source
+
+            soup = BeautifulSoup(page, 'html.parser')
+            results = soup.find_all(class_='gsc_a_tr')
 
 
-                soup = BeautifulSoup(page, 'html.parser')
-                results = soup.find_all(class_='gsc_a_tr')
-
-def make_pretty(html_code)
+# def make_pretty(html_code)
 
 
 if __name__ == '__main__':
@@ -106,7 +108,7 @@ if __name__ == '__main__':
     saveDir = "data/" + str(datetime.now().strftime("%m.%d.%Y_%H:%M:%S")) + ".csv"
     get_urls(queries, saveDir)
 
-    get_info()
+    get_info(saveDir)
 
     # Take existing dataframe URLs and get csv entries
     # get_info()
